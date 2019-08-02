1d60eab22ef65ee1228ae3b1af39727fcef96f61
Carlos Perez <carlos@ioncom.com.mx>
2019-08-02 00:19:58 -0500

    ajuste del al calse prototipos

--- clase-21.js
+++ clase-21.js
@@ -7,10 +7,6 @@ Persona.prototype.saludar = () => {
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)  
 }
 
-Persona.prototype.soyAlto = function(){
-  return this.altura > 1.8
-}
-
 var sacha = new Persona('Sacha','Lifszyc')
 var erika = new Persona('Erika', 'Luna')
 var arturo = new Persona('Arturo', 'Martinez')
